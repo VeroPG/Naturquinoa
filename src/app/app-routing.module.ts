253cd59e9b729832ca5a6a23e50b3f322895a6ed
“Veronica <vero.parragraca@gmail.com>
2024-01-16 10:22:17 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ListaComponent } from "./lista/lista.component";
+import { RecetasComponent } from "./recetas/recetas.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/search", pathMatch: "full" },
+  { path: "recetas", component: RecetasComponent },
+  { path: "lista", component: ListaComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatChipsModule } from "@angular/material/chips";
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FormularioComponent } from "./formulario/formulario.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CalculadoraImcComponent } from "./calculadora-imc/calculadora-imc.component";
+import { CarruselExperienciasComponent } from "./carrusel-experiencias/carrusel-experiencias.component";
+import { ReproductorComponent } from "./reproductor/reproductor.component";
+import { RecetasComponent } from "./recetas/recetas.component";
+import { ListaComponent } from "./lista/lista.component";
+import { HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    FormularioComponent,
+    CalculadoraImcComponent,
+    CarruselExperienciasComponent,
+    ReproductorComponent,
+    RecetasComponent,
+    ListaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
